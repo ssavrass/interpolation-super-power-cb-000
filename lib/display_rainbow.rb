1c357aaaf7a7631d5e144a53e3499c2776b8a4f8
Sergey Savrasov <ssavrass@gmail.com>
2017-02-09 16:26:42 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,16 @@
 # Write your #display_rainbow method here
+def display_rainbow(array)
+  printed_array = ""
+
+    array.each do |x|
+
+      if array.index(x) == array.count - 1
+         printed_array << x[0].upcase + ": " + x
+      else
+        printed_array << x[0].upcase + ": " + x + ", "
+      end
+
+    end
+
+  puts printed_array
+end
